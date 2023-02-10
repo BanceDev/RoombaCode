c529cd2a6daf5af84a00e700cd43d448e6bfdbfe
BanceDev <lanceaborden@gmail.com>
2023-02-10 11:17:04 -0500

    progess on Exploration 1 maze

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
-#include <FEHIO.h>
-#include <FEHLCD.h>
 #include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHLCD.h>
+#include <FEHIO.h>
 
-int main(void)
-{
-    AnalogInputPin CdS_cell(FEHIO::P0_0);
-    
-    while(true) {
-        LCD.Clear(BLACK);
-        LCD.Write(CdS_cell.Value());
-        Sleep(0.2);
-    }
+
+int main(void) {
+
+    //TODO: swap naming
+    DigitalInputPin backLeft(FEHIO::P0_0);
+    DigitalInputPin backRight(FEHIO::P0_1);
+    DigitalInputPin frontLeft(FEHIO::P0_2);
+    DigitalInputPin frontRight(FEHIO::P3_7);
+
+    FEHMotor rightMotor(FEHMotor::Motor0,9.0);
+    FEHMotor leftMotor(FEHMotor::Motor1,9.0);
+
+    rightMotor.SetPercent(25);
+    leftMotor.SetPercent(25);
+
+    Sleep(1.0);  
+
+    rightMotor.Stop();
+    leftMotor.Stop();
+
+
+
     return 0;
 }
--- main.d
+++ main.d
@@ -1,13 +1,14 @@
-../main.d: ../main.cpp Libraries/FEHIO.h Startup/derivative.h \
- Startup/MK60DZ10.h Drivers/adc16.h Libraries/FEHLCD.h Drivers/spi.h \
- ../fehproteusfirmware/Startup/derivative.h Libraries/FEHUtility.h \
- Libraries/LCDColors.h
-Libraries/FEHIO.h:
+../main.d: ../main.cpp Libraries/FEHUtility.h Libraries/FEHMotor.h \
+ Startup/derivative.h Startup/MK60DZ10.h Libraries/FEHLCD.h Drivers/spi.h \
+ ../fehproteusfirmware/Startup/derivative.h Libraries/LCDColors.h \
+ Libraries/FEHIO.h Drivers/adc16.h
+Libraries/FEHUtility.h:
+Libraries/FEHMotor.h:
 Startup/derivative.h:
 Startup/MK60DZ10.h:
-Drivers/adc16.h:
 Libraries/FEHLCD.h:
 Drivers/spi.h:
 ../fehproteusfirmware/Startup/derivative.h:
-Libraries/FEHUtility.h:
 Libraries/LCDColors.h:
+Libraries/FEHIO.h:
+Drivers/adc16.h:
