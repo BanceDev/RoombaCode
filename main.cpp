af7c1bf219e826241b6f9fe46e19aec518a70dad
BanceDev <lanceaborden@gmail.com>
2023-02-17 09:18:45 -0500

    base drive train code

--- main.cpp
+++ main.cpp
@@ -3,11 +3,6 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 
-#define LEFT 1
-#define RIGHT 0
-#define FRONT 0
-#define BACK 1
-
 // class for the Drive train and its functions
 class DriveTrain {
     private:
@@ -16,16 +11,10 @@ class DriveTrain {
         // cries in lack of modular capabilities TwT
         FEHMotor rightMotor = FEHMotor(FEHMotor::Motor0,9.0);
         FEHMotor leftMotor = FEHMotor(FEHMotor::Motor1,9.0);
-        DigitalInputPin backLeft = DigitalInputPin(FEHIO::P0_2);
-        DigitalInputPin backRight = DigitalInputPin(FEHIO::P3_0);
-        DigitalInputPin frontLeft = DigitalInputPin(FEHIO::P0_0);
-        DigitalInputPin frontRight = DigitalInputPin(FEHIO::P0_1);
 
     public:
         DriveTrain();
         void DriveStraight(int speed);
-        void TurnUntilBumper(int dir, int side, int speed);
-        void DriveUntilBumper(int side, int speed);
         void StopDriving();
 
 };
@@ -48,56 +37,12 @@ void DriveTrain::StopDriving() {
 }
 
 
-// Drives straight until both bumpers are pressed
-// side can be front or back
-// 0 for front bumpers 1 for back bumpers
-void DriveTrain::DriveUntilBumper(int side, int speed) {
-    DriveStraight(speed);
-    
-    if (side == 0) {
-        while(frontLeft.Value() || frontRight.Value()){}
-        StopDriving();
-    } else if (side == 1) {
-        while(backLeft.Value() || backRight.Value()){}
-        StopDriving();
-    }
-
-}
-
-// Turns until both bumpers are pressed
-// dir can be left or right wheel and side can be front or back
-// 0 for right wheel and 1 for left wheel
-// 0 for front bumpers 1 for back bumpers
-void DriveTrain::TurnUntilBumper(int dir, int side, int speed) {
-    if (dir == 0) {
-        rightMotor.SetPercent(speed);
-    } else if (dir == 1) {
-        leftMotor.SetPercent(speed);
-    }
-
-    if (side == 0) {
-        while(frontLeft.Value() || frontRight.Value()){}
-        StopDriving();
-    } else if (side == 1) {
-        while(backLeft.Value() || backRight.Value()){}
-        StopDriving();
-    }
-}
-
 
 
 int main(void) {
 
     //Initialize the Drive Train
     DriveTrain driveTrain;
-
-    //Run the drive routine for the maze
-    driveTrain.DriveUntilBumper(FRONT, 25);
-    driveTrain.TurnUntilBumper(RIGHT, BACK, -25);
-    driveTrain.DriveUntilBumper(FRONT, 25);
-    driveTrain.TurnUntilBumper(LEFT, BACK, -25);
-    driveTrain.DriveUntilBumper(FRONT, 25);
-
     
     return 0;
 }
