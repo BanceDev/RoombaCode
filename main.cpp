bbe378544d118fdf3149338b639ee229b8b0b9aa
BanceDev <lanceaborden@gmail.com>
2023-02-17 11:14:27 -0500

    Exploration 2
    Co-authored-by: hinton224 <hinton224@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -22,12 +22,21 @@ class DriveTrain {
         // cries in lack of modular capabilities TwT
         FEHMotor rightMotor = FEHMotor(FEHMotor::Motor0,9.0);
         FEHMotor leftMotor = FEHMotor(FEHMotor::Motor1,9.0);
+        DigitalEncoder rightEncoder = DigitalEncoder(FEHIO::P0_0);
+        DigitalEncoder leftEncoder = DigitalEncoder(FEHIO::P0_1);
+
+        AnalogInputPin rightSensor = AnalogInputPin(FEHIO::P2_0);
+        AnalogInputPin middleSensor = AnalogInputPin(FEHIO::P2_1);
+        AnalogInputPin leftSensor = AnalogInputPin(FEHIO::P2_2);
+        
 
     public:
         DriveTrain();
         void DriveStraight(int speed);
         void StopDriving();
         void FollowLine();
+        void DriveTurn(int speed1, int speed2);
+        void DriveDistance(float distance, int speed);
 
 };
 
@@ -42,11 +51,30 @@ void DriveTrain::DriveStraight(int speed) {
     leftMotor.SetPercent(speed);
 }
 
+// funtion to drive a given distance
+void DriveTrain::DriveDistance(float distance, int speed) {
+    leftEncoder.ResetCounts();
+    rightEncoder.ResetCounts();
+    while (leftEncoder.Counts() < (distance * 40.5) && rightEncoder.Counts() < (distance * 40.5)) {
+        DriveStraight(speed);
+    }
+    StopDriving();
+}
+
+
+void DriveTrain::DriveTurn(int speed1, int speed2) {
+    // drive both wheels at given speed
+    rightMotor.SetPercent(speed1);
+    leftMotor.SetPercent(speed2);
+}
+
 // function to stop both wheels 
 void DriveTrain::StopDriving() {
     rightMotor.Stop();
     leftMotor.Stop();
 }
+
+
 
 // function to follow a line
 void DriveTrain::FollowLine() {
@@ -59,25 +87,26 @@ void DriveTrain::FollowLine() {
         // If I am in the middle of the line...
 
             case MIDDLE:
-            // Set motor powers for driving straight
+                DriveStraight(25);
+                // Set motor powers for driving straight
 
                 /* Drive */
-                if ( /* Right sensor is on line */ ) {
-
+                if (rightSensor.Value() > 2.3) {
                     state = RIGHT; // update a new state
-
                 }
-            /* Code for if left sensor is on the line */
+                /* Code for if left sensor is on the line */
+                if (leftSensor.Value() > 2.3) {
+                    state = LEFT; // update to left
+                }
                 break;
 
             // If the right sensor is on the line...
             case RIGHT:
-            // Set motor powers for right turn
-            /* Drive */
-                if( /* I no longer need to turn right… */ ) {
-
-                /* update a new state */
-
+                DriveTurn(0,25);
+                // Set motor powers for right turn
+                /* Drive */
+                if(rightSensor.Value() < 1.5) {
+                    state = MIDDLE;
                 }
 
                 break;
@@ -85,7 +114,12 @@ void DriveTrain::FollowLine() {
             // If the left sensor is on the line...
             case LEFT:
             /* Mirror operation of RIGHT state */
-
+                DriveTurn(25,0);
+                // Set motor powers for right turn
+                /* Drive */
+                if(leftSensor.Value() > 1.5) {
+                    state = MIDDLE;
+                }
                 break;
 
             default: // Error. Something is very wrong.
@@ -94,7 +128,8 @@ void DriveTrain::FollowLine() {
 
         }
 
-    // Sleep a bit
+        // Sleep a bit
+        Sleep(0.1);
 
     }
 
@@ -108,6 +143,7 @@ int main(void) {
 
     //Initialize the Drive Train
     DriveTrain driveTrain;
+    driveTrain.DriveDistance(14, 25);
     
     return 0;
 }
